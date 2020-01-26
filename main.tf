e223c7184c8b5361c9ae42e3e91986a0915f6774
Evan Harris <evancharris1@Gmail.com>
2020-01-26 09:20:12 -0500

    Test terraform GitHub Actions

---
+++ main.tf
@@ -0,0 +1,15 @@
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a web server
+resource "digitalocean_droplet" "web-test" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-test-1"
+  region = "nyc3"
+  size   = "s-4vcpu-8gb"
+  ssh_keys = [
+    26055817
+  ]
+}
