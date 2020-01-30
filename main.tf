1d286cfcfa66e0c0be294bb4f146c9220a64e554
Evan Harris <evancharris1@Gmail.com>
2020-01-30 18:40:29 -0500

    Extend terraform deployment workflow

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
-# Configure the DigitalOcean Provider
+# Variables
+variable "pvt_key" {}
+variable "buddy_webhook_token" {}
+variable "ssh_user" {}
+
+# Initialize the DigitalOcean Provider
 provider "digitalocean" {}
+
+# Create a new tag
+resource "digitalocean_tag" "prod" {
+  name = "prod"
+}
 
 # Create a web server
 resource "digitalocean_droplet" "web-test" {
@@ -10,4 +20,42 @@ resource "digitalocean_droplet" "web-test" {
   ssh_keys = [
     26055817
   ]
+  tags = [digitalocean_tag.prod.id]
+}
+
+# Allow access to meta data for the DO droplet
+data "digitalocean_droplet" "web-test" {
+  name = "generated-mesh-droplet"
+  depends_on = [
+    digitalocean_droplet.web-test
+  ]
+}
+
+# Bootstrap the server
+resource "null_resource" "seed" {
+  depends_on = [
+    digitalocean_droplet.web-test, # this line can be removed as the web-test data node is already a dependent of the DO resource
+    data.digitalocean_droplet.web-test
+  ]
+
+  connection {
+    user        = var.ssh_user
+    type        = "ssh"
+    private_key = file(var.pvt_key)
+    timeout     = "2m"
+    host        = data.digitalocean_droplet.web-test.ipv4_address
+  }
+
+  provisioner "file" {
+    source      = "pipeline.sh"
+    destination = "/tmp/pipeline.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/pipeline.sh",
+      # Launch the buddy pipeline
+      "/tmp/pipeline.sh ${var.buddy_webhook_token} ${data.digitalocean_droplet.web-test.ipv4_address}",
+    ]
+  }
 }
---
+++ pipeline.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl --location --request GET 'https://app.buddy.works/evancharris1/meshrelay-com/pipelines/pipeline/229813/trigger-webhook?token='${1}'&DROPLET_IP='${2}
