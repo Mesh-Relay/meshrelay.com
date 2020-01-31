5954b74ccad2f047acdc4c70ebe4c422c6a2db71
Evan Harris <evancharris1@Gmail.com>
2020-01-30 20:35:58 -0500

    Pass droplet id to buddy pipeline

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@ variable "pvt_key" {}
 variable "buddy_webhook_token" {}
 variable "ssh_user" {}
 
-# Initialize the DigitalOcean Provider
+# Initialize the DigitalOcean (DO) Provider
 provider "digitalocean" {}
 
-# Create a new tag
+# Create DO tags
 resource "digitalocean_tag" "prod" {
   name = "prod"
+}
+
+resource "digitalocean_tag" "blue" {
+  name = "blue"
 }
 
 # Create a web server
@@ -20,7 +24,7 @@ resource "digitalocean_droplet" "web-test" {
   ssh_keys = [
     26055817
   ]
-  tags = [digitalocean_tag.prod.id]
+  tags = [digitalocean_tag.prod.id, digitalocean_tag.blue.id]
 }
 
 # Allow access to meta data for the DO droplet
@@ -54,7 +58,7 @@ resource "null_resource" "seed" {
     inline = [
       "chmod +x /tmp/pipeline.sh",
       # Launch the buddy pipeline
-      "/tmp/pipeline.sh ${var.buddy_webhook_token} ${data.digitalocean_droplet.web-test.ipv4_address}",
+      "/tmp/pipeline.sh ${var.buddy_webhook_token} ${data.digitalocean_droplet.web-test.ipv4_address} ${data.digitalocean_droplet.web-test.id}",
     ]
   }
 }
